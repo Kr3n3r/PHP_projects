7d01e5de5be073f55471bf8a0b1e31b8aee8d25e
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-02-08 14:41:02 +0100

    +gestionusuarios

--- utils.php
+++ utils.php
@@ -30,6 +30,6 @@ $user="sakila";
 $password="sakila";
 $dbname="sakila";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-or die ('Could not connect to the database server' . mysqli_connect_error());
+// $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+// or die ('Could not connect to the database server' . mysqli_connect_error());
 ?>
