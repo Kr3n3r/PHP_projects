78a4110562d923e856632dc40c9deb799506374b
Kr3n3r <marmolromeroalejandro@gmail.com>
2022-02-10 12:16:16 +0100

    inicio

---
+++ ejerciciosBasicos/tabla_multiplicar_2.php
@@ -0,0 +1,12 @@
+<?php
+echo "<table>";
+for ($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
+    echo "<td>5*$i</td>";
+    $resultado=5*$i;
+    echo "<td>$resultado</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
+    
---
+++ holaMundo/index.php
@@ -0,0 +1,12 @@
+<?php
+    $txt="Hola mundo";
+    $numero=10;
+    echo "Hola Mundo $txt";
+    echo "<br>";
+    
+    echo "Mi número es el ". ($numero + $numero) ." .";
+    echo "<br>";
+    
+    $numero+=$numero;
+    echo "Mi número es el ";
+?>
